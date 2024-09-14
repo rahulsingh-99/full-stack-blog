fa8af83c84ccf2d3575c460fc1d1858d215c6a51
rahulsingh-99 <rahul.rock.rr99@gmail.com>
2024-09-14 23:33:49 +0530

    commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
@@ -6,7 +6,7 @@ import { connectToDb, verifyUserDetails } from '@/lib/helpers';
 import prisma from '@/prisma';
 import bcrypt from 'bcrypt';
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID as string,
